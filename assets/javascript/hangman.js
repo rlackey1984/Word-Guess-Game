e0538b9a64caa8c5506b7726290c74f076b070db
rlackey1984 <rafael.lackey@gmail.com>
2018-04-07 09:09:58 -0500

    I added a file

---
+++ assets/javascript/hangman.js
@@ -0,0 +1,174 @@
+window.onload = function () {
+
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var categories;         // Array of topics
+    var chosenCategory;     // Selected catagory
+    var getHint;          // Word getHint
+    var word;              // Selected word
+    var guess;             // Geuss
+    var geusses = [];      // Stored geusses
+    var lives;             // Lives
+    var counter;           // Count correct geusses
+    var space;              // Number of spaces in word '-'
+
+    // Get elements
+    var showLives = document.getElementById("mylives");
+    var showCatagory = document.getElementById("scatagory");
+    var getHint = document.getElementById("hint");
+    var showClue = document.getElementById("clue");
+
+
+
+    // create alphabet ul
+    var buttons = function () {
+        myButtons = document.getElementById('buttons');
+        letters = document.createElement('ul');
+
+        for (var i = 0; i < alphabet.length; i++) {
+            letters.id = 'alphabet';
+            list = document.createElement('li');
+            list.id = 'letter';
+            list.innerHTML = alphabet[i];
+            check();
+            myButtons.appendChild(letters);
+            letters.appendChild(list);
+        }
+    }
+
+
+    // Select Catagory
+    var selectCat = function () {
+        if (chosenCategory === categories[0]) {
+            catagoryName.innerHTML = "Planets in Our Solar System";
+        } else if (chosenCategory === categories[1]) {
+            catagoryName.innerHTML = "Famous People";
+        } else if (chosenCategory === categories[2]) {
+            catagoryName.innerHTML = "Cities";
+        }
+    }
+
+    // Create geusses ul
+    result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (word[i] === "-") {
+                guess.innerHTML = "-";
+                space = 1;
+            } else {
+                guess.innerHTML = "_";
+            }
+
+            geusses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
+
+    // Show lives
+    comments = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+            showLives.innerHTML = "Game Over";
+        }
+        for (var i = 0; i < geusses.length; i++) {
+            if (counter + space === geusses.length) {
+                showLives.innerHTML = "You Win!";
+            }
+        }
+    }
+
+    // Animate man
+    var animate = function () {
+        var drawMe = lives;
+        drawArray[drawMe]();
+    }
+
+
+
+    
+
+
+
+    // OnClick Function
+    check = function () {
+        list.onclick = function () {
+            var geuss = (this.innerHTML);
+            this.setAttribute("class", "active");
+            this.onclick = null;
+            for (var i = 0; i < word.length; i++) {
+                if (word[i] === geuss) {
+                    geusses[i].innerHTML = geuss;
+                    counter += 1;
+                }
+            }
+            var j = (word.indexOf(geuss));
+            if (j === -1) {
+                lives -= 1;
+                comments();
+                animate();
+            } else {
+                comments();
+            }
+        }
+    }
+
+
+    // Play
+    play = function () {
+        categories = [
+            ["jupiter", "Mars", "Mercury", "Pluto", "Earth", "Saturn", "Uranus"],
+            ["barack-obama", "cristiano-ronaldo", "angelina-jolie", "muhammad-ali", "albert-einstein"],
+            ["delhi", "shanghai", "tokyo", "dc", "London"]
+        ];
+
+        chosenCategory = categories[Math.floor(Math.random() * categories.length)];
+        word = chosenCategory[Math.floor(Math.random() * chosenCategory.length)];
+        word = word.replace(/\s/g, "-");
+        console.log(word);
+        buttons();
+
+        geusses = [];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        result();
+        comments();
+        selectCat();
+        canvas();
+    }
+
+    play();
+
+    // Hint
+
+    hint.onclick = function () {
+
+        hints = [
+            ["Biggest", "Kratos", "Closest to the Sun", "Is This a Planet", "You are Here", "If you like it you should", "Lol"],
+            ["44th President of the United States", "Portuguese professional footballer", "American actress, filmmaker, and humanitarian", "Olympic and World Champion boxer", "The most celebrated scientist of the Twentieth Century"],
+            ["Why do people keep asking for sandwiches", "Once Upon a Time In", "3rd Fast and Furious", "Merica", "Bridge is Falling Down"]
+        ];
+
+        var catagoryIndex = categories.indexOf(chosenCategory);
+        var hintIndex = chosenCategory.indexOf(word);
+        showClue.innerHTML = "Clue: - " + hints[catagoryIndex][hintIndex];
+    };
+
+    // Reset
+
+    document.getElementById('reset').onclick = function () {
+        correct.parentNode.removeChild(correct);
+        letters.parentNode.removeChild(letters);
+        showClue.innerHTML = "";
+        context.clearRect(0, 0, 400, 400);
+        play();
+    }
+};
